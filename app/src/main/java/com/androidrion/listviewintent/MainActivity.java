0b327f451bb5037b7b6b4f8822daffb256499b8a
rionymous_ <callmerion24@gmail.com>
2019-10-25 05:01:16 +0700

    Initial commit

---
+++ app/src/main/java/com/androidrion/listviewintent/MainActivity.java
@@ -0,0 +1,59 @@
+package com.androidrion.listviewintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list);
+        String[] values = new String[]{
+                "Facebook", "Instagram", "Twitter", "Tumblr", "LINE", "VK",
+                "Pinterest", "Reddit", "Foursquare", "YouTube", "LinkedIn", "Quora",
+                "Flickr", "Twitch", "GitHub", "Behance", "ok.ru", "Steam",
+                "Vine", "KakaoTalk", "Telegram", "SnapChat", "Viber", "WhatsApp"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent myIntent = new Intent(view.getContext(), FacebookActivity.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 1) {
+                    Intent myIntent = new Intent(view.getContext(), FacebookActivity.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 2) {
+                    Intent myIntent = new Intent(view.getContext(), FacebookActivity.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 3) {
+                    Intent myIntent = new Intent(view.getContext(), FacebookActivity.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+            }
+        });
+    }
+}
